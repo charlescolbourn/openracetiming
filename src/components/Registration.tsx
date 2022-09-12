39408ca90c313ff86692886cd6925d2faa559981
charlescolbourn <charles.colbourn@and.digital>
2022-09-12 16:28:48 +0100

    load and display csv registration file. Logo in header

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,74 +1,28 @@
 import * as React from 'react';
-import {
-  View,
-  Text,
-  Button,
-  SafeAreaView,
-  TouchableOpacity,
-} from 'react-native';
-import Dialog from 'react-native-dialog';
-// import RNFS from 'react-native-fs';
-import { DownloadDirectoryPath, readDir, readFile } from 'react-native-fs';
+import { View, Text, Button } from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
 
 const Registration = () => {
-  const [showFileDialog, setShowFileDialog] = React.useState(false);
-
-  const [files, setFiles] = React.useState([]);
-
   const [fileData, setFileData] = React.useState('');
-
-  const getDirContent = async (path) => {
-    // 		const reader = RNFS.readDir(path).then( (result) => {
-    // 		    return Promise.all([RNFS.stat(result[0].path), result[0].path]);
-    // 		    }).then( (fileList) => {
-    // 		        setFileData(JSON.stringify(fileList));
-    //
-    //
-    // 		}).catch( (e) => Alert.alert(e.message));
-    const reader = await readDir(path);
-    //  		setFileData(JSON.stringify(reader));
-    setFiles(reader);
-  };
-
-  const readFileContent = async (filename) => {
-    const response = await readFile(DownloadDirectoryPath + '/' + filename);
-    setFileData(response); //set the value of response to the fileData Hook.
-  };
 
   return (
     <View>
       <Text>
         <Button
           title="Import csv"
-          onPress={() => {
-            getDirContent(DownloadDirectoryPath);
-            setShowFileDialog(true);
+          onPress={async () => {
+            try {
+              const response = await DocumentPicker.pick({
+                presentationStyle: 'fullScreen',
+              });
+              const file = await fetch(response[0].uri);
+              const data = await file.text(); //JSON.stringify(file);
+              setFileData(data);
+            } catch (e) {
+              setFileData('ERROR:' + e.message);
+            }
           }}
         />
-        <Dialog.Container visible={showFileDialog}>
-          <Dialog.Title>import file</Dialog.Title>
-          <Dialog.Description>
-            <SafeAreaView>
-              {files.map(({ name }) => (
-                <TouchableOpacity
-                  key={name}
-                  onPress={() => {
-                    setShowFileDialog(false);
-                    readFileContent(name);
-                  }}
-                >
-                  <Text>{name}</Text>
-                </TouchableOpacity>
-              ))}
-            </SafeAreaView>
-          </Dialog.Description>
-          <Dialog.Button
-            label="Cancel"
-            onPress={() => {
-              setShowFileDialog(false);
-            }}
-          />
-        </Dialog.Container>
       </Text>
       <Text>{fileData}</Text>
     </View>
