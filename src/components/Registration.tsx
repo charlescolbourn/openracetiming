ccd7022cb586217392f11d40e02cd5ba5e40803d
charlescolbourn <charles.colbourn@and.digital>
2022-09-13 16:52:50 +0100

    working csv parsing

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,9 +1,37 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
+import { DataTable } from 'react-native-paper';
+import { readString } from 'react-native-csv';
 
 const Registration = () => {
   const [fileData, setFileData] = React.useState('');
+  const [parsedData, setParsedData] = React.useState([]);
+
+  const parseCSV = (data) => {
+    const results = readString(data, {});
+    return results.data; //handle errors
+  };
+
+  const headerLine = (record) => {
+    return (
+      <DataTable.Header>
+        {record.map((field) => {
+          return <DataTable.Title key={field}>{field}</DataTable.Title>;
+        })}
+      </DataTable.Header>
+    );
+  };
+
+  const rowsLine = (record) => {
+    return (
+      <DataTable.Row>
+        {record.map((field) => {
+          return <DataTable.Cell key={field}>{field}</DataTable.Cell>;
+        })}
+      </DataTable.Row>
+    );
+  };
 
   return (
     <View>
@@ -17,13 +45,21 @@ const Registration = () => {
               });
               const file = await fetch(response[0].uri);
               const data = await file.text(); //JSON.stringify(file);
-              setFileData(data);
+              setParsedData(parseCSV(data));
             } catch (e) {
               setFileData('ERROR:' + e.message);
             }
           }}
         />
       </Text>
+      <View>
+        <DataTable>
+          {parsedData[0] ? headerLine(parsedData[0]) : ''}
+          {parsedData[0]
+            ? parsedData.slice(1).map((record) => rowsLine(record))
+            : ''}
+        </DataTable>
+      </View>
       <Text>{fileData}</Text>
     </View>
   );
