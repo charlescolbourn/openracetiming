f9b19f5e3eb6af0a195036dc55ea1602b852f08b
charlescolbourn <charles.colbourn@and.digital>
2022-09-14 16:56:57 +0100

    basic add/edit form

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,67 +1,150 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
 import { DataTable } from 'react-native-paper';
 import { readString } from 'react-native-csv';
 
 const Registration = () => {
-  const [fileData, setFileData] = React.useState('');
   const [parsedData, setParsedData] = React.useState([]);
+  const [firstname, setFirstname] = React.useState('');
+  const [lastname, setLastname] = React.useState('');
+  const [club, setClub] = React.useState('');
+  const [category, setCategory] = React.useState('');
+  const [addOrEdit, setAddOrEdit] = React.useState(false);
+  const [displaySaveButton, setDisplaySaveButton] = React.useState(false);
+  const [statusMessage, setStatusMessage] = React.useState('');
 
   const parseCSV = (data) => {
-    const results = readString(data, {});
+    const results = readString(data, { header: true });
     return results.data; //handle errors
   };
 
-  const headerLine = (record) => {
+  const headerLine = (keys) => {
     return (
       <DataTable.Header>
-        {record.map((field) => {
+        {keys.map((field) => {
           return <DataTable.Title key={field}>{field}</DataTable.Title>;
         })}
       </DataTable.Header>
     );
   };
 
+  const selectRecord = (record) => {
+    setFirstname(record.Forename);
+    setLastname(record.Surname);
+    setClub(record.Club);
+    setCategory(record.Category);
+    setAddOrEdit(true);
+    setDisplaySaveButton(true);
+  };
+
+  const keyFromObject = (obj) => {
+    return obj[Object.keys(obj)[0]] + obj[Object.keys(obj)[1]];
+  };
+
   const rowsLine = (record) => {
     return (
-      <DataTable.Row>
-        {record.map((field) => {
-          return <DataTable.Cell key={field}>{field}</DataTable.Cell>;
-        })}
-      </DataTable.Row>
+      <TouchableOpacity
+        key={keyFromObject(record)}
+        onPress={() => selectRecord(record)}
+      >
+        <DataTable.Row key={keyFromObject(record)}>
+          {Object.values(record).map((field) => {
+            return <DataTable.Cell key={field}>{field}</DataTable.Cell>;
+          })}
+        </DataTable.Row>
+      </TouchableOpacity>
     );
   };
 
   return (
     <View>
-      <Text>
-        <Button
-          title="Import csv"
-          color="#e69138ff"
-          onPress={async () => {
-            try {
-              const response = await DocumentPicker.pick({
-                presentationStyle: 'fullScreen',
-              });
-              const file = await fetch(response[0].uri);
-              const data = await file.text(); //JSON.stringify(file);
-              setParsedData(parseCSV(data));
-            } catch (e) {
-              setFileData('ERROR:' + e.message);
-            }
-          }}
-        />
-      </Text>
       <View>
-        <DataTable>
-          {parsedData[0] ? headerLine(parsedData[0]) : ''}
-          {parsedData[0]
-            ? parsedData.slice(1).map((record) => rowsLine(record))
-            : ''}
-        </DataTable>
+        <Text>
+          <Button
+            title="Import csv"
+            color="#e69138ff"
+            onPress={async () => {
+              try {
+                const response = await DocumentPicker.pick({
+                  presentationStyle: 'fullScreen',
+                });
+                const file = await fetch(response[0].uri);
+                const data = await file.text(); //JSON.stringify(file);
+                setParsedData(parseCSV(data));
+              } catch (e) {
+                setStatusMessage('ERROR:' + e.message);
+              }
+            }}
+          />
+        </Text>
+        <View>
+          <DataTable>
+            {parsedData.length > 0
+              ? headerLine(Object.keys(parsedData[0]))
+              : ''}
+            {parsedData.length > 0
+              ? parsedData.map((record) => rowsLine(record))
+              : ''}
+          </DataTable>
+        </View>
+        {addOrEdit ? (
+          <View backgroundColor="#AAAAAAAA">
+            <Text>Firstname: </Text>
+            <TextInput placeholder="Firstname" value={firstname} />
+            <Text>Lastname: </Text>
+            <TextInput placeholder="Lastname" value={lastname} />
+            <Text>Club: </Text>
+            <TextInput placeholder="Club" value={club} />
+            <Text>Category: </Text>
+            <TextInput placeholder="Category" value={category} />
+          </View>
+        ) : (
+          ''
+        )}
       </View>
-      <Text>{fileData}</Text>
+      <View>
+        <Text>
+          {!displaySaveButton ? (
+            <Button
+              title="Add entry"
+              color="#e69138ff"
+              onPress={async () => {
+                setAddOrEdit(true);
+                setCategory('');
+                setFirstname('');
+                setLastname('');
+                setClub('');
+                setDisplaySaveButton(true);
+              }}
+            />
+          ) : (
+            ''
+          )}
+          {displaySaveButton ? (
+            <Button title="Save" color="#e69138ff" onPress={async () => {}} />
+          ) : (
+            ''
+          )}
+          {displaySaveButton ? (
+            <Button
+              title="Cancel"
+              color="#e69138ff"
+              onPress={async () => {
+                setAddOrEdit(false);
+                setCategory('');
+                setFirstname('');
+                setLastname('');
+                setClub('');
+                setDisplaySaveButton(false);
+              }}
+            />
+          ) : (
+            ''
+          )}
+        </Text>
+      </View>
+      <Text>{statusMessage}</Text>
     </View>
   );
 };
