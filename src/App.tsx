3a4bc0e3bc44234c5c5d38ac173e27b1ebf5c834
charlescolbourn <charles.colbourn@and.digital>
2022-10-06 18:25:38 +0100

    WIP adding settings page & settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,83 +2,25 @@
 
  */
 
-import Timing from './components/Timing';
-import Identify from './components/Identify';
-import Registration from './components/Registration';
+import Settings from './pages/Settings';
+// import TimeRace from './pages/TimeRace';
 
 import * as React from 'react';
-import { View, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import {  Image } from 'react-native';
 
-//eslint-disable-next-line
-function RegistrationScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Registration />
-    </View>
-  );
-}
-
-//eslint-disable-next-line
-function TimingScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Timing />
-    </View>
-  );
-}
-
-//eslint-disable-next-line
-function IdentifyScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Identify />
-    </View>
-  );
-}
-
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 200, height: 80 }}
-      source={require('./ORT_Header.png')}
-    />
-  );
-}
-
-const Tab = createBottomTabNavigator();
+// function LogoTitle() {
+//   return (
+//     <Image
+//       style={{ width: 200, height: 80 }}
+//       source={require('./ORT_Header.png')}
+//     />
+//   );
+// }
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Registration"
-          component={Registration}
-          options={{
-            headerStyle: { height: 80 },
-            headerTitle: (props) => <LogoTitle {...props} />,
-          }}
-        />
-        <Tab.Screen
-          name="Timing"
-          component={Timing}
-          options={{
-            headerStyle: { height: 80 },
-            headerTitle: (props) => <LogoTitle {...props} />,
-          }}
-        />
-        <Tab.Screen
-          name="Identify"
-          component={Identify}
-          options={{
-            headerStyle: { height: 80 },
-            headerTitle: (props) => <LogoTitle {...props} />,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    /* <TimeRace/> */
+    <Settings />
   );
 };
 
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,65 @@
+/**
+
+ */
+
+import React from 'react';
+
+import { View, Button, TextInput } from 'react-native';
+import { DataTable } from 'react-native-paper';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Settings = () => {
+  const [availableRaces, setAvailableRaces] = React.useState([]);
+  const [raceData, setRaceData] = React.useState([]);
+
+  const initialiseFromLocalStorage = () => {
+    getRacesFromLocalStorage().then((raceList) => {
+      let listTable = raceList
+        .sort((a, b) => a.date < b.date)
+        .map((race) => {
+          console.log(race);
+        });
+      setAvailableRaces(listTable);
+    });
+  };
+
+  React.useEffect(() => initialiseFromLocalStorage());
+
+  const addNewRace = () => {
+    console.log('foo');
+  };
+  const getRacesFromLocalStorage = () => {
+    return AsyncStorage.getItem('@ORT_allraces');
+  };
+  const updateTextField = (key, text) => {
+    setRaceData({ ...raceData, key: raceData[key] + text });
+  };
+
+  return (
+    <>
+      <View>
+        <DataTable>{availableRaces}</DataTable>
+
+        <Button
+          onPress={() => {
+            addNewRace();
+          }}
+          title="add race"
+        />
+      </View>
+
+      <View>
+        <TextInput
+          //         key={key}
+          //         editable={false}
+          //         placeholder={key}
+          //         value={value}
+          onChangeText={(changedText) => updateTextField(changedText)}
+        />
+      </View>
+    </>
+  );
+};
+
+export default Settings;
