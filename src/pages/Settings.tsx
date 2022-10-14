9cd8f228b145166295c070a80dde1670d75f04c4
charlescolbourn <charles.colbourn@and.digital>
2022-10-14 10:39:19 +0100

    adding values to race data works ok

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -4,21 +4,32 @@
 
 import React from 'react';
 
-import { View, Button, TextInput, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Button,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Switch,
+} from 'react-native';
 import { DataTable } from 'react-native-paper';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
 
 const Settings = () => {
   const [availableRaces, setAvailableRaces] = React.useState([]);
-  const [raceData, setRaceData] = React.useState([]);
+  const [raceData, setRaceData] = React.useState({ massStart: true });
+
   const [datePickerVisible, setDatePickerVisible] = React.useState(false);
+  const [showNewRaceForm, setShowNewRaceForm] = React.useState(false);
+  const [debug, setDebug] = React.useState('');
 
   const initialiseFromLocalStorage = () => {
     getRacesFromLocalStorage().then((raceList) => {
       let listTable = raceList
-        .sort((a, b) => a.date < b.date)
+        .sort((a, b) => a.raceDate < b.raceDate)
         .map((race) => {
           console.log(race);
         });
@@ -28,58 +39,79 @@ const Settings = () => {
 
   React.useEffect(() => initialiseFromLocalStorage());
 
-  const addNewRace = () => {
-    console.log('foo');
+  const saveRace = () => {
+    setDebug(JSON.stringify(raceData));
   };
+
   const getRacesFromLocalStorage = () => {
     return AsyncStorage.getItem('@ORT_allraces');
   };
-  const updateTextField = (key, text) => {
-    setRaceData({ ...raceData, key: raceData[key] + text });
+  const updateField = (key, value) => {
+    let newObj = raceData;
+    newObj[key] = value;
+    setRaceData(newObj);
+    setDebug(JSON.stringify(raceData));
+  };
+
+  const newRaceForm = () => {
+    return (
+      <View>
+        <Text>{debug}</Text>
+        <TextInput
+          //         key={key}
+          //         editable={false}
+          placeholder="Race Name"
+          onChangeText={(changedText) => updateField('raceName', changedText)}
+        />
+
+        <TouchableOpacity onPress={() => setDatePickerVisible(true)}>
+          <Text>
+            {raceData.raceDate
+              ? moment(raceData.raceDate).format('DD/MM/YYYY')
+              : 'Click to set race date'}
+          </Text>
+        </TouchableOpacity>
+        <DateTimePickerModal
+          isVisible={datePickerVisible}
+          mode="date"
+          onConfirm={(date) => {
+            //                 let thisRace = raceData;
+            //                 thisRace['raceDate'] = date.getTime();
+            //                 setRaceData(thisRace);
+            setDatePickerVisible(false);
+            updateField('raceDate', date.getTime());
+          }}
+          onCancel={() => setDatePickerVisible(false)}
+        />
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={raceData.massStart ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={(val) => updateField('massStart', val)}
+          value={raceData.massStart}
+        />
+
+        <Button
+          onPress={() => {
+            saveRace();
+          }}
+          title="add race"
+        />
+      </View>
+    );
   };
 
   return (
     <>
       <View>
         <DataTable>{availableRaces}</DataTable>
-
-        <Button
-          onPress={() => {
-            addNewRace();
-          }}
-          title="add race"
-        />
       </View>
 
-      <View>
-        <TextInput
-          //         key={key}
-          //         editable={false}
-          placeholder="Race Name"
-          onChangeText={(changedText) =>
-            updateTextField('racename', changedText)
-          }
-        />
-
-        <TouchableOpacity onPress={() => setDatePickerVisible(true)}>
-          <Text>Choose race date</Text>
-        </TouchableOpacity>
-        <DateTimePickerModal
-          isVisible={datePickerVisible}
-          mode="date"
-          onConfirm={() => setDatePickerVisible(false)}
-          onCancel={() => setDatePickerVisible(false)}
-        />
-
-        <TextInput
-          //         key={key}
-          //         editable={false}
-          placeholder="Mass start"
-          onChangeText={(changedText) =>
-            updateTextField('massstart', changedText)
-          }
-        />
-      </View>
+      {showNewRaceForm ? (
+        newRaceForm()
+      ) : (
+        <Button onPress={() => setShowNewRaceForm(true)} title="new race" />
+      )}
     </>
   );
 };
