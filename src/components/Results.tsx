1866ec458b77385c280e52753cd54cd39b03f329
charlescolbourn <charles.colbourn@and.digital>
2022-11-03 13:37:49 +0000

    results display kind of works but doesn't display all results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,33 +1,66 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
+import { DataTable } from 'react-native-paper';
 import LocalStorage from '../lib/LocalStorage';
 import Utils from '../lib/Utils';
 import moment from 'moment';
+import EntrantRecordLine from './EntrantRecordLine';
 
 const Results = () => {
-  const [resultsData, setResultsData] = React.useState('');
+  const [resultsData, setResultsData] = React.useState([]);
   const [currentRace, setCurrentRace] = React.useState({});
+  const [debug, setDebug] = React.useState('');
 
   React.useEffect(() => {
     if (!currentRace || Object.keys(currentRace).length === 0) {
-      LocalStorage.getCurrentRace().then((raceDetails) => {
-        setCurrentRace(JSON.parse(raceDetails));
-      });
+      LocalStorage.getCurrentRace()
+        .then((raceDetails) => {
+          setCurrentRace(JSON.parse(raceDetails));
+        })
+        .catch((e) => setDebug(e.message));
     }
   });
 
   const displayResultsFromLocalContent = () => {
-    LocalStorage.getResults(Utils.getRaceKey(currentRace)).then((results) => {
-      setResultsData(results);
+    LocalStorage.getResults(Utils.getRaceKey(currentRace))
+      .then((results) => {
+        formatResultsTable(JSON.parse(results));
+      })
+      .catch((e) => setDebug(e.message));
+    // LocalStorage.getResults(Utils.getRaceKey(currentRace)).then( (results) =>
+    // {
+    // const resultsObject = JSON.parse(results);
+    // setDebug(JSON.stringify(Object.keys(resultsObject)));
+    //
+    // }).catch ( (e) => setDebug(e.message));
+  };
+
+  const formatResultsTable = (results) => {
+    Object.keys(results).map((key) => {
+      //setDebug(key);
+      return LocalStorage.getEntrant(Utils.getRaceKey(currentRace), key)
+        .then((entrantRecordString) => {
+          let extendedEntrantRecord = JSON.parse(entrantRecordString);
+          extendedEntrantRecord.finishtime = moment(results[key]).format(
+            'HH:mm:ss.S'
+          );
+          setResultsData([
+            ...resultsData,
+            <EntrantRecordLine record={extendedEntrantRecord} />,
+          ]);
+          //setDebug(JSON.stringify(extendedEntrantRecord));
+        })
+        .catch((e) => setDebug(e.message));
     });
   };
 
-  React.useEffect(() => {
-    displayResultsFromLocalContent();
-  });
+  //   React.useEffect(() => {
+  //     displayResultsFromLocalContent();
+  //   });
 
   return (
     <View>
+      <Text>{debug}</Text>
       <Text>
         {currentRace && Object.keys(currentRace).length > 0
           ? `${currentRace.raceName} ${moment(currentRace.raceDate).format(
@@ -37,7 +70,7 @@ const Results = () => {
 
         {/*                         <CurrentRaceView raceDetails={currentRace}/>   */}
       </Text>
-      <Text>{JSON.stringify(resultsData)}</Text>
+      <DataTable>{resultsData}</DataTable>
       <Button title="reload" onPress={() => displayResultsFromLocalContent()} />
     </View>
   );
