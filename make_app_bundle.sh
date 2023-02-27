d0de25468a0a38c1e011fb8fa7429bb21713aba6
charlescolbourn <charles.colbourn@and.digital>
2023-02-27 14:26:57 +0000

    added build config

---
+++ make_app_bundle.sh
@@ -0,0 +1,1 @@
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle && cd ./android && ./gradlew app:assembleRelease
